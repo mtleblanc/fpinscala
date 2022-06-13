55008ec29ede57e92967b26310144bda229c341d
Michael LeBlanc <leblanc.michael@gmail.com>
2022-06-12 17:37:53 -0700

    Ch 2.  Getting Started

--- src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
@@ -33,7 +33,12 @@ object MyProgram:
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = 
+    @annotation.tailrec
+    def go(n: Int, prev: Int, cur: Int): Int =
+      if n == 0 then cur
+      else go(n-1, cur, prev + cur)
+    go(n, 1, 0)
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) =
@@ -121,7 +126,13 @@ object PolymorphicFunctions:
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = 
+    @annotation.tailrec
+    def loop(n: Int): Boolean =
+      if n > as.length - 2 then true
+      else if gt(as(n), as(n+1)) then false
+      else loop(n+1)
+    loop(0)
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -134,13 +145,13 @@ object PolymorphicFunctions:
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    a => b => f(a,b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    f(_)(_)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -155,5 +166,5 @@ object PolymorphicFunctions:
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
 
